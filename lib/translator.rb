8bbb3e64e4a8466c6c49ec4f5c31a3936e70a643
Alice Ly Ms <alicehly@gmail.com>
2019-07-11 14:55:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,14 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+    library=yaml.load_file('/lib/emoticons.yml')
+  
+  put library.inspect
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
